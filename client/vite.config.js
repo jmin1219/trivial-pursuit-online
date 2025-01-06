1209d8eb23005ef4e816d32ed3f572186dbf91e5
Jaymin <60022152+jmin1219@users.noreply.github.com>
2025-01-06 14:25:00 +0900

    Fix: Correct vite.config.js plugin import

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,5 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+};
