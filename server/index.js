36c1e27a1beee47f8b66e1c4fc264308bc8f76ab
Jaymin <60022152+jmin1219@users.noreply.github.com>
2025-01-06 14:13:39 +0900

    deployment edit

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use("/api/games", gameApiRoutes);
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+  app.use(express.static(path.join(__dirname1, "client", "dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
