7a91d06d203d53c65d817229c83a619376bacac2
Jaymin <60022152+jmin1219@users.noreply.github.com>
2025-01-07 10:33:09 +0900

    cors configuration test

--- server/index.js
+++ server/index.js
@@ -40,6 +40,7 @@ const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "../client", "dist")));
+  console.log(__dirname1);
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "../client", "dist", "index.html"));
