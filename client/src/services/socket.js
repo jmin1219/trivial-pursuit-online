11934645f7fc1c09b86dd716f890a2cb09c3a631
Jaymin <60022152+jmin1219@users.noreply.github.com>
2025-11-17 16:20:02 -0800

    Refactor code structure for improved readability and maintainability

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-const clientSocket = io(SOCKET_URL);
+const clientSocket = io("https://trivial-pursuit-online.onrender.com", {
+  transports: ["websocket", "polling"],
+});
 
 export default clientSocket;
